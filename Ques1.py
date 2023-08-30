db588aa2f310322928f9e00a4326b4c674343881
Ankush0103 <110992910+Ankush0103@users.noreply.github.com>
2023-08-30 23:24:58 +0530

    Add files via upload

--- Ques1.py
+++ Ques1.py
@@ -1,28 +1,21 @@
-'''Josh went to the market to buy N apples. He found two shops, shop A and B, where
-apples were being sold in lots. He can buy any number of the complete lot(s) but not loose apples. He is confused with the price and wants you to figure out the minimum cost to buy exactly N apples. Write an algorithm for Josh to calculate the minimum cost to buy exactly N apples.
-Input Format: The first line of the input consists of an integer – N, representing the total number of apples that Josh wants to buy.
-The second line consists of two space-separated positive integers – M1 and P1, representing the number of apples in a lot and the lot’s price at shop A, respectively.
-The third line consists of two space-separated positive integers-M2 and P2, representing the number of apples in a lot and lot’s price at shop B, respectively.
-Output Format: Print a positive integer representing the minimum price at which Josh can buy the apples.
-Sample Input:
-        19
-        3 10
-        4 15
-        Sample Output:
-        65'''
-
-n = int(input())
-m1, p1 = map(int, input().split())
-m2, p2 = map(int, input().split())
-price = -1
+# ASCII Reverse Order to print correct passowrd
+# Input-'796115110113721110141108'
+# Output - PrepInsta
+ 
+s = "796115110113721110141108"
+s = s[::-1]
+print(s)
 i = 0
-while(m1*i<=n):
-    rem = n - m1*i
-    if(rem%m2==0):
-        cost = int(p1*i + p2*(rem/m2))
-        if(cost<price or price == -1):
-            price = cost 
-    i+=1
-if(price!=-1):
-    print(price)
-    
+res = ""
+while(i<len(s)-1):
+    x = s[i] + s[i+1]
+    if x == "32":
+        res = res + " "
+    elif int(x) in range(65, 91) or int(x) in range(97, 123):
+        res = res + chr(int(x))
+    elif i+2<len(s):
+        x = x + s[i+2]
+        res = res + chr(int(x))
+        i+=1
+    i+=2
+print(res)
--- Ques2.java
+++ Ques2.java
@@ -1,66 +1,25 @@
-/*A futuristic company is building an autonomous car. The scientists at the company are training the car to perform Reverse parking.
-To park, the car needs to be able to move in backward as well as forward direction. The car is programmed to move backwards B meters and
-forwards again, say F meters, in a straight line. The car does this repeatedly until it is able to park or collides with other objects.
-The car covers 1 meter in T units of time. There is a wall after distance D from cars initial position in the backward direction.
-The car is currently not without defects and hence often hits the wall. The scientists are devising a strategy to prevent this from happening.
-Your task is to help the scientists by providing them with exact information on amount of time available before the car hits the wall.
-Input Format:
-
-First line contains total number of test cases, denoted by N Next N lines, contain a tuple containing 4 values delimited by space F B T D, where 1. F denotes forward displacement in meters 2. B denotes backward displacement in meters 3. T denotes time taken to cover 1 meter 4. D denotes distance from Cars starting position and the wall in backward direction
-Output Format:
-For each test case print time taken by the Car to hit the wall
-Constraints:
-First move will always be in backward direction
-1 <= N <= 100
-backward displacement > forward displacement i.e. (B > F)
-forward displacement (F) > 0
-backward displacement (B) > 0
-time (T) > 0
-distance (D) > 0
-All input values must be positive integers only
-
-Sample Input 0
-
-2
-6 9 3 18
-3 7 5 20
-Sample Output 0
-
-162
-220*/
-
-import java.util.*;
+/*
+int[] arr = {3, 4, 5};
+number of rotations, k = 2
+k = 1-> {5, 3, 4}
+k = 2-> {4, 5, 3} Array index at 1 and 2 is 5 and 3.
+ */
 public class Ques2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        //int n = sc.nextInt();
-        int f = sc.nextInt();
-        int b = sc.nextInt();
-        int t = sc.nextInt();
-        int d = sc.nextInt();
-        int td = 0; // Total distance
-        int tt = 0; // Total time
-        /*while(td<d)
-        {
-            tt += b*t;   // 6 9 3 18   , 3 7 5 20
-            td += b;
-            if(td>=d)
-                break;
-            tt += f*t;
-            td -= f;
-        }
-        System.out.println(tt);*/
-        /*for(int i = 0; i<n; i++)
-        {*/
-            while(b<d)
-            {
-                td=td+b+f;
-
-                d=d-(b-f);
+        int[] arr = {3, 4, 5};
+        int n = arr.length;
+        int last = 0;
+        int k = 2;
+        for(int j = 0; j<k; j++) {
+            for (int i = 0; i < n; i++) {
+                last = arr[n - 1];
             }
-            td=td+d;
-
-        //}
-        System.out.println(td*t);
+            for (int i = n-1; i > 0; i--) {
+                arr[i] = arr[i - 1];
+            }
+            arr[0] = last;
+        }
+        for(int i = 0; i<n; i++)
+            System.out.println(arr[i]);
     }
 }
